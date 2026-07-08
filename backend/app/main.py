8e4397fada567633e909df7875cdd10918e6ce56
danyah <danyah233@gmail.com>
2026-07-08 13:17:07 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.models.customer import Customer
 from app.models.invoice import Invoice
 from app.models.payment import Payment
+from app.models.delivery_company import DeliveryCompany
+
 from app.routers import customers
 from app.routers import invoices
 from app.routers import payments
+from app.routers import delivery_companies
 
 
 Base.metadata.create_all(bind=engine)
@@ -17,9 +21,19 @@ app = FastAPI(
 )
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(customers.router)
 app.include_router(invoices.router)
 app.include_router(payments.router)
+app.include_router(delivery_companies.router)
 
 
 @app.get("/")
