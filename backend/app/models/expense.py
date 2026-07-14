dc4cfcdcc4a87f4a59d57bdeb569be5d055c7f12
danyah <danyah233@gmail.com>
2026-07-14 15:14:53 +0300

    Update expense.py

--- backend/app/models/expense.py
+++ backend/app/models/expense.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.sql import func
 
 from app.database import Base
@@ -10,14 +10,21 @@ class Expense(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     category = Column(String, nullable=False)
+
     amount = Column(Float, nullable=False)
+
     description = Column(String, nullable=True)
+
     receipt_file = Column(String, nullable=True)
 
-    approved_by = Column(Integer, ForeignKey("employees.id"), nullable=True)
+    # سيتم ربطه بجدول Employees لاحقًا
+    approved_by = Column(Integer, nullable=True)
 
     status = Column(String, default="Pending")
 
     is_archived = Column(Boolean, default=False)
 
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
